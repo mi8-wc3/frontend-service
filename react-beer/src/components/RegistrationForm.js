ff193c7a746124cca8b12c9da421e857c4535177
Elena Anosova <anosova.helena3@gmail.com>
2023-08-30 18:08:49 +0200

    #2 Create FE Authentication Page
    Made this form dynamic, so you can sign in or register, if you don't have an acc. Also added more css styles for it.

--- react-beer/src/components/RegistrationForm.js
+++ react-beer/src/components/RegistrationForm.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../styles.css'
+
 const RegistrationForm = () => {
   const [email, setEmail] = React.useState('')
   const [password, setPassword] = React.useState('')
+  const [username, setUsername] = React.useState('')
+  const [isFirstFormActive, setIsFirstFormActive] = React.useState(true)
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value)
@@ -12,11 +15,20 @@ const RegistrationForm = () => {
     setPassword(event.target.value)
   }
 
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handleFormToggle = () => {
+    setIsFirstFormActive(!isFirstFormActive)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
     // Здесь нужно добавить логику отправки данных на сервер
     console.log('Email:', email)
     console.log('Password:', password)
+    console.log('Username:', username)
   }
 
   return (
@@ -31,44 +43,114 @@ const RegistrationForm = () => {
         <img className="cap" src="./beer-pics/cap.png" alt="cap" />
         <img className="cap2" src="./beer-pics/cap2.png" alt="cap2" />
       </div>
-      <div className="registration-window">
-        <h2>Registration</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="input-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-              required
-            />
-          </div>
-          <button type="submit" className="custom-button">
-            <img
-              height={45}
-              width={16}
-              src="./beer-pics/beer-button.png"
-              alt="beer-button"
-            />
-            <img
-              height={45}
-              width={45}
-              src="./beer-pics/bottle-opener-full.png"
-              alt="bottle-opener"
-            />
+      <div
+        className={`login-window ${
+          isFirstFormActive ? '' : 'second-form-active'
+        }`}
+      >
+        <div className="form-toggle">
+          <button
+            onClick={() => setIsFirstFormActive(true)}
+            className={isFirstFormActive ? 'active' : ''}
+          >
+            Sign In
           </button>
-        </form>
+          <button
+            onClick={() => setIsFirstFormActive(false)}
+            className={!isFirstFormActive ? 'active' : ''}
+          >
+            Register
+          </button>
+        </div>
+        {isFirstFormActive ? (
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            <button type="submit" className="custom-button">
+              <img
+                height={45}
+                width={16}
+                src="./beer-pics/beer-button.png"
+                alt="beer-button"
+              />
+              <img
+                height={45}
+                width={45}
+                src="./beer-pics/bottle-opener-full.png"
+                alt="bottle-opener"
+              />
+            </button>
+          </form>
+        ) : (
+          <form>
+            <div className="input-group">
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={handleUsernameChange}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={handleEmailChange}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={handlePasswordChange}
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="custom-button"
+            >
+              <img
+                height={45}
+                width={16}
+                src="./beer-pics/beer-button.png"
+                alt="beer-button"
+              />
+              <img
+                height={45}
+                width={45}
+                src="./beer-pics/bottle-opener-full.png"
+                alt="bottle-opener"
+              />
+            </button>
+          </form>
+        )}
       </div>
     </div>
   )
